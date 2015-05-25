d4c55c0bbc19e8abe3e604aa03ce2357186febea
IveTou <ive_tourinho@hotmail.com>
2015-05-25 19:10:18 -0300

    Primeiro Commit: Notificacao, Estoque e Produto

--- src/graduacao/ufba/lab_engenharia/estoque/BuscaPadraoProduto.java
+++ src/graduacao/ufba/lab_engenharia/estoque/BuscaPadraoProduto.java
@@ -1,17 +1,31 @@
 package graduacao.ufba.lab_engenharia.estoque;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 import graduacao.ufba.lab_engenharia.produto.Produto;
 
 public class BuscaPadraoProduto implements BuscaProdutoBehavior{
 
-
-	public Produto[] buscarProduto(Object[] Args) {
-		//Descrição - Busca um produto por sua chave indentificadora
-		//Argumento - Arg[1] = Chave identificadora do produto
-		//Retorno - Object[] = vetor de todos os produtos que atendem a busca 
+	//Descrição - Busca um produto por sua chave indentificadora
+			//Argumento - Arg[1] = Chave identificadora do produto
+			//Retorno - Object[] = vetor de todos os produtos que atendem a busca 
+			
+	public ArrayList<Produto> buscarProduto(Object[] Args) {
+		HashMap<String,Produto> list_produtos;
+		ArrayList<Produto> produtos = new ArrayList<Produto>() ;
 		
-		// TODO Implementar a busca padrão
-		return null;
+		list_produtos = Estoque.getInstance().getListProdutos();
+		
+		for(Entry<String, Produto> entry : list_produtos.entrySet()) {
+		    Produto produto = entry.getValue();
+		    if(produto.getId() ==1){
+		    	produtos.add(produto);
+		    }
+		}
+		
+		return produtos;
 	}
 
 	
